3ae23895a9b7a4a39c902c9983830e09df0ef489
Sergey Garanin <wwwser11@yandex.ru>
2021-04-27 21:07:41 +0300

    make string smaller

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -9,7 +9,8 @@ def engine(game):
         if qr[0] == qr[1]:
             print('Correct')
         else:
-            print(f"'{qr[0]}' is wrong answer ;(.\nCorrect answer was '{qr[1]}'.)"
+            print(f"'{qr[0]}' is wrong answer ;(."
+                  f"\nCorrect answer was '{qr[1]}'.)"
                   f" Let's try again, {name}!")
             break
     else:
